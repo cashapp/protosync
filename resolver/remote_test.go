b68bef8c407dee18a50a7c3aed2402d76510e140
Yun Lai <ylai@squareup.com>
2021-08-02 10:54:28 +1000

    support parsing ssh short hand repo url with git-url

--- resolver/remote_test.go
+++ resolver/remote_test.go
@@ -1,7 +1,6 @@
 package resolver // nolint: testpackage
 
 import (
-	"net/url"
 	"testing"
 
 	"github.com/pkg/errors"
@@ -11,7 +10,10 @@ import (
 func TestGithubFetcherShouldNotChangeURLScheme(t *testing.T) {
 	t.Parallel()
 	ru := "ssh://git@github.com/cashapp/protosync.git"
-	u, err := url.Parse(ru)
+	repoWithShortURL := &Repo{
+		URL: ru,
+	}
+	u, err := repoWithShortURL.ParseURL()
 	require.NoError(t, err)
 
 	reader, err := githubFetcher(u, "nonexistingcontent", "")
@@ -20,4 +22,38 @@ func TestGithubFetcherShouldNotChangeURLScheme(t *testing.T) {
 
 	require.NotEqual(t, "https", u.Scheme)
 	require.Equal(t, ru, u.String())
+}
+
+func TestGithubFetcherShouldBeOkWithDifferentURLs(t *testing.T) {
+	t.Parallel()
+	urls := []string{
+		"git@github.com:cashapp/protosync.git",
+		"https://ithub.com/cashapp/protosync.git",
+		"ssh://git@github.com/cashapp/protosync.git",
+	}
+
+	for _, us := range urls {
+		repoWithShortURL := &Repo{
+			URL: us,
+		}
+		u, err := repoWithShortURL.ParseURL()
+		require.NoError(t, err)
+
+		reader, err := githubFetcher(u, "nonexistingcontent", "")
+		require.True(t, errors.Is(err, errNotFound))
+		require.Nil(t, reader)
+	}
+}
+
+func TestRepoSSHShortURLParsing(t *testing.T) {
+	t.Parallel()
+	repoWithShortURL := &Repo{
+		URL: "git-1234@github.com:cashapp/protosync.git",
+	}
+	u, err := repoWithShortURL.ParseURL()
+	require.NoError(t, err)
+	require.Equal(t, "ssh", u.Scheme)
+	require.Equal(t, "github.com", u.Host)
+	require.Equal(t, "cashapp/protosync.git", u.Path)
+	require.Equal(t, "git-1234", u.User.Username())
 }
